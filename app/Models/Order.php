d89dcee4f517033f1553efca96edc79d9ab763f7
Lavwin C. Campollo <rave_campus@hotmail.com>
2022-10-18 07:10:10 +0800

    update 5

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $table = 'orders';
+    protected $fillable = [
+        'order_number',
+        'user_id',
+        'full_name',
+        'mobile_number',
+        'delivery_address',
+        'amount',
+        'total',
+        'grand_total',
+        'payment_mode',
+        'status',
+    ];
+
+}
